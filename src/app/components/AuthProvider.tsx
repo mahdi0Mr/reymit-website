66adf735d3ce85a60aa8340d2d59f9bb429d0f4b
mmt <73156888+mahdi0Mr@users.noreply.github.com>
2025-09-01 22:20:48 +0330

    remove tmp

--- src/app/components/AuthProvider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-
-// مطمئن شوید که "export default" وجود دارد
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
