8086ebea1317c53d69e0826441ab6f9ea812d611
mmt <73156888+mahdi0Mr@users.noreply.github.com>
2025-09-02 22:49:56 +0330

    lik shop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default async function HomePage() {
               برای استفاده از تمام قابلیت‌ها لایسنس تهیه کنید و برای دریافت پشتیبانی تیکت ارسال نمایید.
             </p>
             <div className="flex flex-col sm:flex-row gap-4">
-               <a href="https://wumpus.ir/shop" target="_blank" className="flex items-center justify-center gap-2 px-6 py-2 bg-pink-500 text-white font-semibold rounded-lg hover:bg-pink-600 transition">
+               <a href="https://wumpus.ir/product/donatron/" target="_blank" className="flex items-center justify-center gap-2 px-6 py-2 bg-pink-500 text-white font-semibold rounded-lg hover:bg-pink-600 transition">
                 <Gem size={18} /> خرید لایسنس
               </a>
               {/* [تغییر] لینک دیسکورد با لینک ارسال تیکت جایگزین شد */}
