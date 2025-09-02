f04847c4b15f2fe4d0d5cd292fcac6a1cd5c13b7
mmt <73156888+mahdi0Mr@users.noreply.github.com>
2025-09-02 22:43:06 +0330

    fix api version

--- src/app/actions/adminActions.ts
+++ src/app/actions/adminActions.ts
@@ -102,7 +102,7 @@ export async function createAppFile(data: AppFileData) {
         {
           access: "public",
           handleUploadUrl: "/api/admin/upload",
-          // @ts-expect-error
+          // @ts-expect-error: allowOverwrite موجود در type تعریف نشده ولی ما میخوایم استفاده کنیم
           allowOverwrite: true,
         }
       );
